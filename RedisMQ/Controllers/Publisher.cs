7b4baa702da924a9858d668280cefe532565e875
Omar Ashraf <33941687+omarattia3143@users.noreply.github.com>
2021-12-03 04:04:01 +0200

    init

---
+++ RedisMQ/Controllers/Publisher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SharedProject1;
+using StackExchange.Redis;
+
+namespace RedisMQ.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PublisherController : ControllerBase
+    {
+
+        [HttpPost]
+        public void SendRedisMessage(string message)
+        {
+            using var redis = ConnectionMultiplexer
+                .Connect(new ConfigurationOptions
+                {
+                    EndPoints = {"local:6379"},
+                    DefaultDatabase = 1,
+                    AllowAdmin = true,
+                    Ssl = false,
+                    ConnectTimeout = 6000,
+                });
+            var sub = redis.GetSubscriber();
+            var test = new MyTestObject
+            {
+                Id = 10,
+                Message = message
+            };
+
+            var json = JsonSerializer.Serialize(test);
+            
+            sub.Publish("myTestChannel", json);
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using SharedProject1;
+using StackExchange.Redis;
+
+namespace Subscriber
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            using var redis = ConnectionMultiplexer
+                .Connect(new ConfigurationOptions
+                {
+                    EndPoints = { "local:6379" },
+                    DefaultDatabase = 1,
+                    AllowAdmin = true,
+                    Ssl = false,
+                    ConnectTimeout = 6000,
+                });
+            var sub = redis.GetSubscriber();    
+            
+            sub.Subscribe("myTestChannel", (channel, message) => {
+                Console.WriteLine(message);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
